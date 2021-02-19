9baedea99908c14dcb80de7c41ea40a0a91baeb3
MDentro <michelle.dentro1@gmail.com>
2021-02-19 17:23:38 +0100

    uitwerkingen

--- Account.js
+++ Account.js
@@ -1,2 +1,25 @@
 // Export your class here as module.exports = MyClass
-module.exports = null;
+
+class Account {
+    constructor(name, balance, creditLimit) {
+        this.name = name;
+        this.balance = balance;
+        this.creditLimit = creditLimit;
+        this.withDrawLimit = 5000
+    }
+    makeDeposit(amount) {
+        //console.log("storting", amount, amount < 0);
+        if(amount > 0) {
+            this.balance = this.balance + amount;
+        }
+    }
+    makeWithdrawal(amount) {
+        //console.log("withdraw")
+        if(amount < this.balance && amount < this.withDrawLimit) {
+            this.balance = this.balance - amount;
+        }
+    }
+}
+module.exports = Account;
+
+
--- BusinessAccount.js
+++ BusinessAccount.js
@@ -1,2 +1,50 @@
 // Export your class here as module.exports = MyClass
-module.exports = null;
+
+const Account = require("./Account")
+
+class BusinessAccount extends Account {
+    constructor(name, balance, creditLimit, sepaPermission) {
+        super(name, balance, creditLimit);
+        this.withDrawLimit = 20000;
+        this.sepaPermission = sepaPermission;
+
+    }
+    sepaInvoice(amount) {
+        const sepaTransactionCosts = amount * 1 / 100
+        if(amount > 0 && this.sepaPermission === true) {
+            this.balance = this.balance + amount - sepaTransactionCosts;
+        }
+    }
+}
+
+
+// bij alles los (nu uitgecomment om hetzelfde te doen met klasses en overerving
+// class BusinessAccount {
+//     constructor(name, balance, creditLimit, sepaPermission) {
+//         this.name = name;
+//         this.balance = balance;
+//         this.creditLimit = creditLimit;
+//         this.sepaPermission = sepaPermission;
+//     }
+//     makeDeposit(amount) {
+//         if(amount > 0) {
+//             this.balance = this.balance + amount;
+//         }
+//     }
+//     makeWithdrawal(amount) {
+//         const limit = this.balance + this.creditLimit;
+//         if(limit > amount && amount < 20000) {
+//             this.balance = this.balance - amount;
+//         }
+//     }
+//     sepaInvoice(amount) {
+//         const sepaTransactionCosts = amount * 1 / 100
+//         if(amount > 0 && this.sepaPermission === true) {
+//             this.balance = this.balance + amount - sepaTransactionCosts;
+//         }
+//     }
+// }
+
+module.exports = BusinessAccount;
+
+
